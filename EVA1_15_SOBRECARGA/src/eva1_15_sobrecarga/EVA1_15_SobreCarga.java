e661eba3a70573321ed759aaa5ee5b7a5116bb93
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-02-23 22:36:58 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SobreCarga.java
@@ -0,0 +1,21 @@
+/*
+
+ */
+package eva1_15_sobrecarga;
+
+
+public class EVA1_15_SobreCarga {
+
+
+    public static void main(String[] args) {
+      //diferentes println: 10 métodos diferentes
+      
+        System.out.println("Hola");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        
+     //firma de métodos unica para crear sobrecarga
+    }
+    
+}
